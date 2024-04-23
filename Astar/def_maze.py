62fbbe773eebdc15de66aa0f143e2a63d739c9fd
SolusRGB <brandoneddycode@gmail.com>
2024-04-23 15:10:09 +0100

    Refactor maze and calculate_manhattan_distance functions

--- Astar/def_maze.py
+++ Astar/def_maze.py
@@ -3,7 +3,7 @@ maze = [
     [0, 0, 0, 0, 0, 0],
     [0, 1, 0, 1, 0, 0],
     [0, 1, 0, 0, 1, 0],
-    [0, 0, 0, 0, 1, 0]
+    [0, 0, 0, 0, 1, 0],
 ]
 
 maze1 = [
@@ -24,19 +24,28 @@ maze1 = [
     [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 0],
 ]
 
+
 def calculate_manhattan_distance(current_x, current_y, goal_x, goal_y):
     return abs(current_x - goal_x) + abs(current_y - goal_y)
+
 
 # Goal node location
 goal_x, goal_y = 4, 5
 
 # Calculate Manhattan distance for each node
-manhattan_distances = [[calculate_manhattan_distance(x, y, goal_x, goal_y) for y in range(len(maze[0]))] for x in range(len(maze))]
+manhattan_distances = [
+    [calculate_manhattan_distance(x, y, goal_x, goal_y) for y in range(len(maze[0]))]
+    for x in range(len(maze))
+]
 
 
-[[9, 8, 7, 6, 5, 4], 
- [8, 7, 6, 5, 4, 3], 
- [7, 6, 5, 4, 3, 2], 
- [6, 5, 4, 3, 2, 1], 
- [5, 4, 3, 2, 1, 0]]
+[
+    [9, 8, 7, 6, 5, 4],
+    [8, 7, 6, 5, 4, 3],
+    [7, 6, 5, 4, 3, 2],
+    [6, 5, 4, 3, 2, 1],
+    [5, 4, 3, 2, 1, 0],
+]
 
+
+# bnrandfon
