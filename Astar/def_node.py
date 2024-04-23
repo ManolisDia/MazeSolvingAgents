ac91ce59406c0bda145cdb9961cb2f6feeed80e2
SolusRGB <brandoneddycode@gmail.com>
2024-04-23 17:57:26 +0100

    Add new files and update existing files

--- Astar/def_node.py
+++ Astar/def_node.py
@@ -1,37 +1,44 @@
-
 # This file contains the definition of the Node class, which is used to represent nodes in a search tree.
 class Node:
     def __init__(self, coordinates, g, h, f, parent):
-        
+
         self.coordinates = coordinates
-        
-       # Cost from the start node to this node
+
+        # Cost from the start node to this node
         self.g = g
-        
-        #Heuristic - estimated cost from this node to the end node
+
+        # Heuristic - estimated cost from this node to the end node
         self.h = h
-        
+
         # Total cost of the node (f = g + h)
         self.f = f
-        
+
         # Parent node: the node from which this node was reached
         self.parent = parent
-    
-    
+
     def __repr__(self):
         return "<Node {}>".format(self.coordinates)
-    
+
     def expand(self, problem):
         """List the nodes reachable in one step from this node."""
-        return [self.child_node(problem, action)
-                for action in problem.actions(self.coordinates)]
-    
+        return [
+            self.child_node(problem, action)
+            for action in problem.actions(self.coordinates)
+        ]
+
     def child_node(self, problem, action):
         """[Figure 3.10]"""
         next_coordinates = problem.result(self.coordinates, action)
-        next_node = Node(next_coordinates, self, action, problem.path_cost(self.path_cost, self.coordinates, action, next_coordinates))
+        next_node = Node(
+            next_coordinates,
+            self,
+            action,
+            problem.path_cost(
+                self.path_cost, self.coordinates, action, next_coordinates
+            ),
+        )
         return next_node
-    
+
     def solution(self):
         """Return the sequence of actions to go from the root to this node."""
         return [node.action for node in self.path()[1:]]
@@ -43,4 +50,3 @@ class Node:
             path_back.append(node)
             node = node.parent
         return list(reversed(path_back))
-    
--- Astar/def_problem.py
+++ Astar/def_problem.py
@@ -1,5 +1,6 @@
 from def_maze import maze
 
+
 class Problem:
 
     def __init__(self, maze, initial, goal):
@@ -9,23 +10,26 @@ class Problem:
 
     def actions(self, coordinates):
         actions = []
-        directions = [(0, -1), (0, 1), (-1, 0), (1, 0)] 
+        directions = [(0, -1), (0, 1), (-1, 0), (1, 0)]
         for dx, dy in directions:
             next_coordinates = (coordinates[0] + dx, coordinates[1] + dy)
-            if 0 <= next_coordinates[0] < len(self.maze) and 0 <= next_coordinates[1] < len(self.maze[0]) and self.maze[next_coordinates[0]][next_coordinates[1]] == 0:
+            if (
+                0 <= next_coordinates[0] < len(self.maze)
+                and 0 <= next_coordinates[1] < len(self.maze[0])
+                and self.maze[next_coordinates[0]][next_coordinates[1]] == 0
+            ):
                 actions.append((dx, dy))
         return actions
 
     def result(self, coordinates, action):
         return (coordinates[0] + action[0], coordinates[1] + action[1])
 
-
     def goal_test(self, coordinates):
         return coordinates == self.goal
 
     def path_cost(self, c, coordinates1, action, coordinates2):
         return c + 1
-    
+
     def path(self):
         """Return a list of nodes forming the path from the root to this node."""
         node, path_back = self, []
